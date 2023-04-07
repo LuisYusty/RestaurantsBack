48949173c1739a86a208b0e6ead1337e4b731ad8
LuisCardoso <luiscardosoyusty@gmail.com>
2023-04-07 18:10:36 -0500

    estructura arquitectura hexagonal

---
+++ Users/src/main/java/com/BackRestaurants/Users/infrastructure/configuration/BeanConfiguration.java
@@ -0,0 +1,23 @@
+package com.BackRestaurants.Users.infrastructure.configuration;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class BeanConfiguration {
+    /**
+    private final IUserRepository userRepository;
+    private final IUserEntityMapper userEntityMapper;
+
+    @Bean
+    public IUserPersistencePort userPersistencePort() {
+        return new UserJpaAdapter(userRepository, userEntityMapper);
+    }
+
+    @Bean
+    public IUserServicePort objectServicePort() {
+        return new UserUseCase(userPersistencePort());
+    }*/
+}
---
+++ Users/src/test/java/com/BackRestaurants/Users/UsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BackRestaurants.Users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
